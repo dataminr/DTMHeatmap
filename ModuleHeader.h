0b11b3e45f2901628d3816ec2f2f13154e699a72
Cal Stephens <cal@calstephens.tech>
2017-12-16 16:15:52 -0500

    Add ModuleHeader.h

---
+++ ModuleHeader.h
@@ -0,0 +1,22 @@
+//
+//  DTMHeatmap.h
+//  DTMHeatmap
+//
+//  Created by Cal Stephens on 12/16/17.
+//  Copyright © 2017 Bryan Oltman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapHit.h>
+
+//! Project version number for DTMHeatmap.
+FOUNDATION_EXPORT double DTMHeatmapVersionNumber;
+
+//! Project version string for DTMHeatmap.
+FOUNDATION_EXPORT const unsigned char DTMHeatmapVersionString[];
+
+#import "DTMColorProvider.h"
+#import "DTMDiffColorProvider.h"
+#import "DTMDiffHeatmap.h"
+#import "DTMHeatmap.h"
+#import "DTMHeatmapRenderer.h"
